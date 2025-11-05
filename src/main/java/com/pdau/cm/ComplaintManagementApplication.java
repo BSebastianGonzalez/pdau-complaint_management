2f6539add44e841ba2e3e7d226d27c331ffe003f
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-11-05 13:27:48 -0500

    first commit

---
+++ src/main/java/com/pdau/cm/ComplaintManagementApplication.java
@@ -0,0 +1,13 @@
+package com.pdau.cm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintManagementApplication.class, args);
+	}
+
+}
